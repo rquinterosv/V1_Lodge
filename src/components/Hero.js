8081abcece510fb78f56283828e884f0569da0db
rquinterosv <rafael.quinterosv@gmail.com>
2022-10-13 23:10:59 -0300

    Update hero section with Lodge images and colors

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,8 @@ import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import img1 from './img/img1.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
 
 
 const navigation = [
@@ -18,29 +20,31 @@ export default function Example() {
         <div className="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
           <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Data to enrich your</span>{' '}
-                <span className="block text-indigo-600 xl:inline">online business</span>
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl"> 
+
+                <div>
+                    <span className="block xl:inline text-verde">Bienvenidos a</span>
+                </div>
+                <span className="block text-celeste xl:inline">Lodge las Cascadas</span>
               </h1>
-              <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                fugiat veniam occaecat fugiat aliqua.
+              <p className="mt-3 text-base text-azul sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
+                Incertos en un bosque nativo, a orillas del lago Llanquihue y a los pies del volcan Osorno, te invamos a disfurtar de de una experiencia natural
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
                     href="#"
-                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
+                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-verde px-8 py-3 text-base font-medium text-azul hover:bg-azul hover:text-verde md:py-4 md:px-10 md:text-lg"
                   >
-                    Get started
+                    Ubicación
                   </a>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                   <a
                     href="#"
-                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-100 px-8 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 md:py-4 md:px-10 md:text-lg"
+                    className="flex w-full items-center justify-center rounded-md border border-2  bg-white px-8 py-3 text-azul font-medium text-indigo-700 hover:bg-celeste md:py-4 md:px-10 md:text-lg opacity-70"
                   >
-                    Live demo
+                    ¡Contáctanos!
                   </a>
                 </div>
               </div>
@@ -51,7 +55,7 @@ export default function Example() {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full"
-          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+          src={img4}
           alt=""
         />
       </div>
